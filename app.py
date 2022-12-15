041091ce4acd34a6a552ed1143095c4bd3c89783
Alicia <alissipac72@gmail.com>
2022-12-15 12:26:51 -0600

    insertando datos en la base de datos

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from forms import RegistrandoM
 from flask_wtf import CSRFProtect
-
+from datetime import datetime
 from config import Config
 
 
@@ -14,6 +14,8 @@ csrf = CSRFProtect()
 client= MongoClient('localhost', 27017)
 db = client.veterinaria
 insertDate_collection= db.insertDate   ##creando la coleccion
+date= datetime
+
 
 @app.route("/")
 def hello():
@@ -33,25 +35,23 @@ def vistaFormRegistro():
         race= form.race.data
         ownerName= form.ownerName.data
         ownerDni= form.ownerDni.data
+    
+    if request.method== 'POST':
+        petName= form.petName.data
+        datePet= form.datePet.data
+        race= form.race.data
+        ownerName= form.ownerName.data
+        ownerDni= form.ownerDni.data
+        insertDate_collection.insert_one({'petName': petName, 'datePet': datePet, 'race': race, 'ownerName': ownerName, 'ownerDni': ownerDni})
 
         next = request.args.get('next', None)
         if next:
             return redirect(next)
         return redirect(url_for('inicio'))
     return render_template("registrar.html", form= form)
-    # return "Se han registrado los datos"
 
-# @app.route("/registrar", methods=['POST'])
-# def registrar():
-#     petName= request.form['petName']
-#     datePet= request.form['datePet']
-#     race= request.form['race']
-#     ownerName= request.form['ownerName']
-#     ownerDni= request.form['ownerDni']
-#     insertDate_collection.insert_one({'petName': petName, 'datePet': datePet, 'race': race, 'ownerName': ownerName, 'ownerDni': ownerDni})
-#     return "Se ha registrado la mascota"
-#     # insertDate= insertDate_collection.find()
-#     # return render_template('registrar.html', insertDate= insertDate)
+
+
 
 
     
