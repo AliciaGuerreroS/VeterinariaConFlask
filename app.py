9d60e17068a56eea2147270d359245820818195a
Alicia <alissipac72@gmail.com>
2022-12-15 10:15:32 -0600

    Realizando cambios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -9,19 +9,32 @@ db = client.veterinaria
 insertDate_collection= db.insertDate   ##creando la coleccion
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hola, mundo!</h1>"
+def hello():
+    return "HELLO WORLD!"
 
-@app.route("/registrar", methods=['POST'])
-def registrar():
-    petName= request.form['petName']
-    datePet= request.form['datePet']
-    race= request.form['race']
-    ownerName= request.form['ownerName']
-    ownerDni= request.form['ownerDni']
-    insertDate_collection.insert_one({'petName': petName, 'datePet': datePet, 'race': race, 'ownerName': ownerName, 'ownerDni': ownerDni})
-    insertDate= insertDate_collection.find()
-    return render_template('registrar.html', insertDate= insertDate)
+@app.route("/registrarMascota/", methods= ["GET", "POST"])
+def vistaFormRegistro():
+    if request.method == 'POST':
+        petName= request.form['petName']
+        datePet= request.form['datePet']
+        race= request.form['race']
+        ownerName= request.form['ownerName']
+        ownerDni= request.form['ownerDni']
+    return render_template("registrar.html")
+    # return "Se han registrado los datos"
+
+# @app.route("/registrar", methods=['POST'])
+# def registrar():
+#     petName= request.form['petName']
+#     datePet= request.form['datePet']
+#     race= request.form['race']
+#     ownerName= request.form['ownerName']
+#     ownerDni= request.form['ownerDni']
+#     insertDate_collection.insert_one({'petName': petName, 'datePet': datePet, 'race': race, 'ownerName': ownerName, 'ownerDni': ownerDni})
+#     return "Se ha registrado la mascota"
+#     # insertDate= insertDate_collection.find()
+#     # return render_template('registrar.html', insertDate= insertDate)
+
 
     
 
